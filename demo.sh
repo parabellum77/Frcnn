39541c28cc7f17d45e909051afd78e4145d0a4b3
parabellum77 <54352990+parabellum77@users.noreply.github.com>
2022-06-15 05:18:09 +0300

    Delete demo.sh

--- demo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source activate py36
-until python run_webcam.py; do
-    echo "'run_webcam.py' crashed with exit code $?. Restarting..." >&2
-    sleep 1
-done
-source deactivate py36
